d2d7e16ebb0b17b354bcc93796ad79042a8c8469
vmarush <marush228@mail.ru>
2023-05-26 21:57:13 +0300

    добавил поля в  Book, допабил метод __str__ в Genre

--- first/books/models.py
+++ first/books/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
+
 class Book(models.Model):
-    title = models.CharField(max_length=50)
-    autor = models.CharField(max_length=50)
-    year = models.IntegerField()
+    title = models.CharField(max_length=50, blank=True)
+    autor = models.CharField(max_length=50, blank=True)
+    year = models.IntegerField(blank=True)
+    raiting = models.IntegerField(default=0)
+    publisher = models.CharField(max_length=50, null=True, blank=True)
+    genre = models.ForeignKey("Genre", on_delete=models.DO_NOTHING, null=True, blank=True,related_name='books')
 
     def __str__(self):
         # строковое представление объекта
         return f"Книга: {self.id} Название: {self.title} Автор: {self.autor}"
 
+
 class Genre(models.Model):
     title = models.CharField(max_length=50)
+
+    def __str__(self):
+        # строковое представление объекта
+        return f"Жанр {self.id} => {self.title} "
