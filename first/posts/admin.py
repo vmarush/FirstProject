cfe24e2e598b19de03103fc7e56fe631d040aefb
vmarush <marush228@mail.ru>
2023-06-01 00:43:16 +0300

    изменил отображение в Posts

--- first/posts/admin.py
+++ first/posts/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 
 from .models import Post, PostTag, Category
 
-#
-# class PostAdmin(admin.ModelAdmin):
-#     list_display = ("id", "title", "tags", "category", "description")
-#
-#     def get_tags(self, obj):
-#         tags = obj.tags.all()
-#         return "\n".join([t.title for t in tags])
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "get_tags", "category", "description")
+
+    def get_tags(self, obj):
+        tags = obj.tags.all()
+        return "\n".join([t.title for t in tags])
 
 
 admin.site.register(PostTag)
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
 admin.site.register(Category)
